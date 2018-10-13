b5e1e0488d789d2eb534bb055b195e1254191256
Yaniv Nagar <yyyannag@gmail.com>
2018-10-13 21:30:55 +0300

    added /list and /download json file is sorted folders first, then files

--- server/index.js
+++ server/index.js
@@ -1,9 +1,36 @@
+const path = require('path');
+const fs = require('fs');
 const express = require('express');
 const app = express();
 const port = 3000;
 
-app.get('/', (req, res) => res.send('Hello World!'));
+var startFolder = '/Users/trueicecold/Desktop/ttt';
 
-app.use('/static', express.static('/Users/trueicecold/Desktop'));
+app.use('/download', express.static(startFolder));
+app.get('/list/:folderPath?', function(req, res) {
+    var files = {status:1, items:[]};
+    req.params.folderPath = (req.params.folderPath != null) ? req.params.folderPath : '';
+    try {
+        fs.readdirSync(path.join(startFolder, req.params.folderPath)).forEach(file => {
+            if (file.indexOf(".") == 0) return;
+            var item = {name:file};
+            var fstat = fs.statSync(path.join(startFolder, req.params.folderPath, file));
+            item.type = fstat.isDirectory() ? "folder" : "file";
+            item.size = (item.type == "folder") ? "" : fstat.size;
+            files.items.push(item);
+        });
+        files.items.sort(function(a,b) {
+            if (a.type == "file" && b.type == "folder") return 1;
+            if (a.type == "folder" && b.type == "folder") return (a.name < b.name) ? -1 : 1;
+            if (a.type == "file" && b.type == "file") return (a.name < b.name) ? -1 : 1;
+            return 0;
+        });
+    }
+    catch(e) {
+        files.status = 0;
+        files.error = e.code;
+    }
+    res.send(JSON.stringify(files, null, "\t"));
+});
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`));
+app.listen(port, () => console.log(`SwitchDL server is listening on port ${port}!`));
